1bd0ea977b02b757f2a852cfc331b5910559c20a
Kavyan Nayak <kavyannayak104@gmail.com>
2025-08-29 11:42:22 +0530

    basics if-else

---
+++ if-else/1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main()
+{
+    int a, b;
+
+    printf("Enter the First Number :");
+    scanf("%d", &a);
+
+    printf("Enter the Second Number :");
+    scanf("%d", &b);
+
+    if (a > b)
+    {
+        printf("A is Max !");
+    }
+    else{
+        printf("B is Max !");
+    }
+    
+
+}
