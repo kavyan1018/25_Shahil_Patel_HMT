6151696beefd4104875beab0b66c392404349fac
Kavyan Nayak <kavyannayak104@gmail.com>
2025-08-29 10:29:43 +0530

    Addition

---
+++ basics/Oprators/Arithmetic op.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int a = 10;
+    int b = 20;
+
+    int sum = a + b;
+
+    printf("The Sum is %d", sum);
+
+}
---
+++ basics/Oprators/user.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int a, b;
+    int sum;
+
+    printf("Enter the First Number :");
+    scanf("%d", &a);
+
+    printf("Enter the Second Number :");
+    scanf("%d", &b);
+
+    sum = a + b;
+
+    printf("The Sum is : %d", sum);
+}
