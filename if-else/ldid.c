a89d0a3a9e13ba0daa7daed7268e48748a08a9b9
Kavyan Nayak <kavyannayak104@gmail.com>
2025-09-02 10:29:25 +0530

    Ladder if

---
+++ if-else/ldid.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+void main()
+{
+    int a;
+
+    printf("Enter the Number u want to print the Day : \n");
+    scanf("%d",&a);
+
+
+    if (a == 1)
+    {
+        printf("Monday");
+    }
+    if (a == 2)
+    {
+        printf("Tuesday");
+    }
+    if (a == 3)
+    {
+        printf("Wednesday");
+    }
+    if (a == 4)
+    {
+        printf("Thursday");
+    }
+    if (a == 5)
+    {
+        printf("Friday");
+    }
+    if (a == 6)
+    {
+        printf("Saturday");
+    }
+    if (a == 7)
+    {
+        printf("Sunday");
+    }
+
+    
+}
